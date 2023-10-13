1469c7b3b51a2c1805020ec2c14df4258141bee6
haseeb <pkhaseeb04@gmail.com>
2023-10-13 02:27:38 -0700

    practies

---
+++ es6 varibles/practies/script.mjs
@@ -0,0 +1,12 @@
+let myname ="haseeb";
+let mynumber = 220501;
+console.log(myname);
+console.log(mynumber);
+myname ="Muhammad Haseeb"
+mynumber = 25;
+console.log(myname);
+console.log(mynumber);
+let myfathername ="Muhammad Asif";
+console.log(myfathername);
+let mycityname = "peshawar";
+console.log(mycityname);
---
+++ es6 varibles/script.mjs
@@ -0,0 +1,11 @@
+let myNumber = 5;
+console.log(myNumber);
+let myname ="haseeb";
+console.log(myname);
+myNumber = 16;
+console.log(myNumber);
+
+myname = "muhammad haseeb";
+console.log(myname);
+
+
