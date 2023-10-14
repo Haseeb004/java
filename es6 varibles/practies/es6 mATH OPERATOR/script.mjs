67689791f16aa04c3972c98fbca9e3d04c3db468
haseeb <pkhaseeb04@gmail.com>
2023-10-14 05:45:17 -0700

    operators

--- es6 varibles/practies/es6 mATH OPERATOR/script.mjs
+++ es6 varibles/practies/es6 mATH OPERATOR/script.mjs
@@ -2,4 +2,8 @@ let mynum1 = 5;
         let mynum2 = 12;
         console.log(mynum1);
         mynum1 = mynum2 + 5 + mynum1 + 23;
-        console.log(mynum1);        
+        console.log(mynum1);   
+        let mynum =34;
+        let result = 21 + mynum/ 2 + 47 * 3;
+        console.log(result);
+             
